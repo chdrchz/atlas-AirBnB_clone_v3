9b2039f03a4cd17b64e5c1896644e6cf5450de0a
chdrchz <saavannaa.rose@gmail.com>
2024-03-17 17:47:29 -0500

    adding updates I did but dont remember what they were

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,7 +11,7 @@ from models.state import State
 
 
 @app_views.route("/states", methods=['GET'], strict_slashes=False)
-def get_all_states(): 
+def get_all_states():
     """
     This method retrieves a list of all state objects
     Args: states_all - list of all states, keys excluded
