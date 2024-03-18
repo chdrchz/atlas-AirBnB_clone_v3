29f8ce9e4c93249d0e33fa872440222fc091ec9f
chdrchz <saavannaa.rose@gmail.com>
2024-03-18 18:50:33 -0500

    that fixed it! changed keys to be ignored to its own variable, also fixing style guide errors

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -10,7 +10,7 @@ from models import storage
 from models.user import User
 
 
-@app_views.route("/users", methods=['GET'], strict_slashes=False)
+@app_views.route("/users", methods=["GET"], strict_slashes=False)
 def get_all_users():
     """
     This method retrieves a list of all state objects
@@ -23,7 +23,7 @@ def get_all_users():
     return jsonify(users_json)
 
 
-@app_views.route("/users/<user_id>", methods=['GET'], strict_slashes=False)
+@app_views.route("/users/<user_id>", methods=["GET"], strict_slashes=False)
 def get_user(user_id):
     """
     This method retrieves one user object
@@ -33,13 +33,12 @@ def get_user(user_id):
     """
     user = storage.get(User, user_id)
     if user is None:
-        abort(404) #Bad request
+        abort(404)  # Bad request
     user_json = user.to_dict()
     return jsonify(user_json)
 
 
-@app_views.route("/users/<user_id>", methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route("/users/<user_id>", methods=["DELETE"], strict_slashes=False)
 def delete_user(user_id):
     """
     This method deletes one user object
@@ -48,14 +47,14 @@ def delete_user(user_id):
     """
     user = storage.get(User, user_id)
     if user is None:
-        abort(404) #Bad request
+        abort(404)  # Bad request
     storage.delete(user)
     print(f"type of user: {type(user)}")
     storage.save()
-    return jsonify({}), 200 #OK
+    return jsonify({}), 200  # OK
 
 
-@app_views.route("/users", methods=['POST'], strict_slashes=False)
+@app_views.route("/users", methods=["POST"], strict_slashes=False)
 def create_user():
     """
     This method creates a user object
@@ -66,11 +65,11 @@ def create_user():
     """
     json_data = request.get_json(silent=True)
     if not json_data:
-        abort(400, 'Not a JSON')
-    if 'email' not in json_data:
-        abort(400, 'Missing email')
-    if 'password' not in json_data:
-        abort(400, 'Missing password')
+        abort(400, "Not a JSON")
+    if "email" not in json_data:
+        abort(400, "Missing email")
+    if "password" not in json_data:
+        abort(400, "Missing password")
     user = User(**json_data)
     storage.new(user)
     storage.save()
@@ -78,8 +77,7 @@ def create_user():
     return jsonify(user_json), 201
 
 
-@app_views.route("/users/<user_id>", methods=['PUT'],
-                 strict_slashes=False)
+@app_views.route("/users/<user_id>", methods=["PUT"], strict_slashes=False)
 def update_user(user_id):
     """
     This method updates a user object
@@ -90,14 +88,14 @@ def update_user(user_id):
     """
     user = storage.get(User, user_id)
     if not user:
-        abort(404) #Bad request
+        abort(404)  # Bad request
     json_data = request.get_json(silent=True)
     if not json_data:
-        abort(400, 'Not a JSON') #Bad request
-    leave_out_keys = ['id', 'email', 'created_at', 'updated_at']
+        abort(400, "Not a JSON")  # Bad request
+    leave_out_keys = ["id", "email", "created_at", "updated_at"]
     for key, value in json_data.items():
         if key not in leave_out_keys:
             setattr(user, key, value)
     storage.save()
     user_json = user.to_dict()
-    return jsonify(user_json), 200 #OK
+    return jsonify(user_json), 200  # OK
