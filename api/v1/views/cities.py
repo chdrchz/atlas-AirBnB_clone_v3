83db1b5714abe56b5f93380a557ebaf31280d536
chdrchz <saavannaa.rose@gmail.com>
2024-03-18 10:13:56 -0500

    added get_city and delete_city methods

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,13 +1,55 @@
 #!/usr/bin/python3
 """
 This module creates the view for all city objects
-as well as handles all default api functions
+and handles all default api actions
 """
 
 from api.v1.views import app_views
 from flask import Flask, abort, jsonify, request
 from models import storage
+from models.city import City
 from models.state import State
 
-# @app_views.route("/states/<state_id>/cities")
-# def get_all_cities_from_state_id():
+
+@app_views.route("/states/<state_id>/cities", methods=['GET'], strict_slashes=False)
+def get_cities(state_id):
+    """
+    This method retrieves a list of all cities in one state
+    Args: cities - contains a list all cities in one state
+    Return: a list dictionaries with cities and 
+    """
+    cities = storage.get_all(City, {"state_id": state_id})
+    if not cities:
+        abort(404)
+    cities_json = [city.to_dict() for city in cities]
+    return jsonify(cities_json)
+
+
+@app_views.route("/cities/<city_id>", methods=['GET'], strict_slashes=False)
+def get_city(city_id):
+    """
+    This method retrieves one city object
+    Args: city - contains one city object, based on its city id
+          city_json - city object converted to a dictionary
+    Return: a json dictionary containing one city object
+    """
+    city = storage.get(City, city_id)
+    if not city: 
+        abort(404) #Bad request
+    city_json = city.to_dict()
+    return jsonify(city_json)
+
+
+@app_views.route("/cities/<city_id>", methods=['GET'], strict_slashes=False)
+def delete_city(city_id):
+    """
+    This method deletes a city object
+    Args: city - contains one city object, based on its city id
+    Return: an empty dictionary
+    """
+    city = storage.get(City, city_id)
+    if not city: 
+        abort(404) #Bad request
+    storage.delete(city)
+    storage.save()
+    return jsonify({}), 200 #OK
