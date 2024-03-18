ed9fa9aab147360addf07e374f7a152d9e6132dd
root <root@DESKTOP-FADRK94>
2024-03-18 10:09:37 -0500

    pep8

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -10,7 +10,7 @@ from models import storage
 from models.state import State
 
 
-@app_views.route("/states", methods=['GET'], strict_slashes=False)
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
 def get_all_states():
     """
     This method retrieves a list of all state objects
@@ -24,7 +24,7 @@ def get_all_states():
     return jsonify(states_json)
 
 
-@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
 def get_state(state_id):
     """
     This method retrieves one state object
@@ -34,12 +34,12 @@ def get_state(state_id):
     """
     state = storage.get(State, state_id)
     if state is None:
-        abort(404) #Bad request
+        abort(404)  # Bad request
     state_json = state.to_dict()
     return jsonify(state_json)
 
 
-@app_views.route("/states/<state_id>", methods=['DELETE'], strict_slashes=False)
+@app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
 def delete_state(state_id):
     """
     This method deletes one state object
@@ -49,15 +49,15 @@ def delete_state(state_id):
     # print("here")
     state = storage.get(State, state_id)
     if state is None:
-        abort(404) #Bad request
+        abort(404)  # Bad request
     storage.delete(state)
     storage.save()
-    return jsonify({}), 200 #OK
-    #^^^ not 200? -Ace
+    return jsonify({}), 200  # OK
+    # ^^^ not 200? -Ace
     # Nvm yeah I went ahead and changed it. -Ace
 
 
-@app_views.route("/states", methods=['POST'], strict_slashes=False)
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
 def create_state():
     """
     This method creates a state object
@@ -67,16 +67,16 @@ def create_state():
     json_data = request.get_json(silent=True)
     if not json_data:
         print("here")
-        abort(400, 'Not a JSON') #Bad request
-    if 'name' not in json_data:
-        abort(400, 'Missing name') #Bad request
+        abort(400, "Not a JSON")  # Bad request
+    if "name" not in json_data:
+        abort(400, "Missing name")  # Bad request
     state = State(**json_data)
     state.save()
     state_json = state.to_dict()
-    return jsonify(state_json), 201 #OK
+    return jsonify(state_json), 201  # OK
 
 
-@app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
 def update_state(state_id):
     """
     This method updates a state object
@@ -88,13 +88,13 @@ def update_state(state_id):
     state = storage.get(State, state_id)
     json_data = request.get_json(silent=True)
     if not state:
-        abort(404) #Bad request
+        abort(404)  # Bad request
     if not json_data:
-        abort(400, 'Not a JSON') #Bad request
-    if 'name' not in json_data:
-        abort(400, 'Missing name') #Bad request
+        abort(400, "Not a JSON")  # Bad request
+    if "name" not in json_data:
+        abort(400, "Missing name")  # Bad request
     for key, value in json_data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ["id", "created_at", "updated_at"]:
             setattr(state, key, value)
     state.save()
     state_json = state.to_dict()
