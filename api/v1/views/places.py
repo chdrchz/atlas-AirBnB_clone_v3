4ea7562c0cdaf76c96b73cb1f43385ccfad47aee
chdrchz <saavannaa.rose@gmail.com>
2024-03-18 19:09:39 -0500

    checking if city od exists before querying the data

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -21,13 +21,13 @@ def get_places(city_id):
           places - contains all place objects in one city
     Return: a list of dictionaries containing places
     """
-    city = storage.get(City, city_id)
-    if not city:
-        abort(404)
-    places = [place.to_dict() for place in city.places]
-    if not places:
-        return jsonify([]), 200
-    return jsonify(places), 201
+    if city_id:
+        city = storage.get(City, city_id)
+        if city is not None:
+            places = [place.to_dict() for place in city.places]
+            return jsonify(places)
+        else:
+            return abort(404)
 
 
 @app_views.route("/places/<place_id>", methods=["GET"],
