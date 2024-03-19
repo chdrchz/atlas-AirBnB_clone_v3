b2446b94439fcff82a043fb95545d725cea8bd68
chdrchz <saavannaa.rose@gmail.com>
2024-03-18 20:17:23 -0500

    adding additional review methods

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -9,6 +9,7 @@ from flask import Flask, abort, jsonify, make_response, request
 from models import storage
 from models.review import Review
 from models.place import Place
+from models.user import User
 
 
 @app_views.route("/places/<place_id>/reviews", methods=["GET"],
@@ -44,3 +45,48 @@ def get_reviews(review_id):
         abort(404)
     review_json = review.to_dict()
     return jsonify(review_json)
+
+
+@app_views.route("/reviews/<review_id>", methods=["DELETE"],
+                 strict_slashes=False)
+def delete_review(review_id):
+    """
+    This method deletes a review
+    Args: review - contains one review object, based on its id
+    Return: empty dictionary in json format
+    """
+    review = storage.get(Review, review_id)
+    if not review:
+        abort(404)
+    storage.delete(review)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route("/places/<place_id>/reviews", methods=["POST"],
+                 strict_slashes=False)
+def create_review(place_id):
+    """
+    This method creates a review
+    Args: review - contains one review object, based on its id
+          user - contains user
+    Return: empty dictionary in json format
+    """
+    json_data = request.get_json()
+    if not json_data:
+        abort(400, 'Not a JSON') # Bad request
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404) # Bad request
+    if "user_id" not in json_data.keys():
+        abort(400, "Missing user_id") # Bad request
+    user = storage.get(User, json_data.get("user_id"))
+    if not user:
+        abort(404) # Bad request
+    if "text" not in json_data.keys():
+        abort(400, "Missing text")
+    json_data["place_id"] = place_id
+    review = Review(**json_data)
+    review_json = review.to_dict()
+    return jsonify(review_json), 201 # OK
+
