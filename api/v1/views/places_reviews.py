d78f129829f06c3a5e6d4d1ceb96f1eb2fb9c8e4
chdrchz <saavannaa.rose@gmail.com>
2024-03-18 19:49:35 -0500

    testing get all reviews method

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -8,6 +8,21 @@ from api.v1.views import app_views
 from flask import Flask, abort, jsonify, make_response, request
 from models import storage
 from models.review import Review
+from models.place import Place
 
 
-
+@app_views.route('/places/<place_id>/reviews', methods=['GET'], strict_slashes=False)
+def get_reviews(place_id):
+    """
+    This method retrieves a list of all reviews for a place
+    Args: place - contains a place object
+    Return: a list of dictionaries containing reviews
+    """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404) # Bad request
+    reviews = place.reviews
+    reviews_list = []
+    for review in reviews:
+        reviews_list.append(review.to_dict())
+    return jsonify(reviews_list)
